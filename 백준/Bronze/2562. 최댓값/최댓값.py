636fc9f64a1fe684683c80c4e1022dd46f77bb56
이혜연 <139879153+HyeYeonLee0112@users.noreply.github.com>
2026-03-07 22:19:52 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -2,18 +2,23 @@
 # 문제 링크: https://www.acmicpc.net/problem/2562
 import sys #인풋을 더 빠르게 하는 메서드를 지원하는 모듈
 
-#list = sys.stdin.readline()
+#list = sys.stdin.readline() #
 myList = []
 
 for line in sys.stdin: #입력 개수는 상수가 아님
-    myList.append(int(line.strip()))  
-    #strip메서드로 줄바꿈 문자 '\n제거' 후 string에서 int형으로
+    myList.append(int(line))  
+    # strip메서드로 줄바꿈 문자 '\n'제거가 필요할 줄 알았으나
+    # int()가 자동으로 공백과 개행문자 제거
 
-max, maxIndex = 0, 0
+maxNum, maxIndex = 0, 0
 
+'''기존 코드
 for i in range(len(myList)):
     if(myList[i] > max):
         max = myList[i]
         maxIndex = i
+'''
+maxNum = max(myList) 
+maxIndex = myList.index(maxNum)
 
-print(max, maxIndex+1)
+print(maxNum, maxIndex+1)
